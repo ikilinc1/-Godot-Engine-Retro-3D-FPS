3f5e126535c2379ec566e00e5703f2389c22e3bd
ikilinc1 <ikilinc1@binghamton.edu>
2022-04-25 14:37:00 -0400

    set up state setup

--- Retro-3D-FPS/characters/enemies/Monster.gd
+++ Retro-3D-FPS/characters/enemies/Monster.gd
@@ -1,5 +1,8 @@
 extends KinematicBody
 
+onready var anim_player = $Graphics/AnimationPlayer
+onready var health_manager = $HealthManager
+
 enum STATES {IDLE, CHASE, ATTACK, DEAD}
 var cur_state = STATES.IDLE
 
@@ -9,7 +12,47 @@ func _ready():
 		for child in bone_attachment.get_children():
 			if child is HitBox:
 				child.connect("hurt", self, "hurt")
+				
+	health_manager.connect("dead", self, "set_state_dead")
+	set_state_idle()
 
+
+func _process(delta):
+	match cur_state:
+		STATES.IDLE:
+			process_state_idle(delta)
+		STATES.CHASE:
+			process_state_chase(delta)
+		STATES.ATTACK:
+			process_state_attack(delta)
+		STATES.DEAD:
+			process_state_dead(delta)
+			
+func set_state_idle():
+	cur_state = STATES.IDLE
+	anim_player.play("idle_loop")
+	
+func set_state_chase():
+	cur_state = STATES.CHASE
+	
+func set_state_attack():
+	cur_state = STATES.ATTACK
+	
+func set_state_dead():
+	cur_state = STATES.DEAD
+	anim_player.play("die")
+	
+func process_state_idle(delta):
+	pass
+	
+func process_state_chase(delta):
+	pass
+	
+func process_state_attack(delta):
+	pass
+	
+func process_state_dead(delta):
+	pass
 
 func hurt(damage: int, dir: Vector3):
-	pass
+	health_manager.hurt(damage, dir)
