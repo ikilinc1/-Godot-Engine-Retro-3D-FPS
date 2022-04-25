deb151d56320bf6d93b12f6e1c9c9999debd3b9b
ikilinc1 <ikilinc1@binghamton.edu>
2022-04-25 16:11:42 -0400

    added enemy movement and facing

--- Retro-3D-FPS/characters/enemies/Monster.gd
+++ Retro-3D-FPS/characters/enemies/Monster.gd
@@ -2,13 +2,17 @@ extends KinematicBody
 
 onready var anim_player = $Graphics/AnimationPlayer
 onready var health_manager = $HealthManager
+onready var character_mover = $CharacterMover
+onready var nav : Navigation = get_parent()
 
 enum STATES {IDLE, CHASE, ATTACK, DEAD}
 var cur_state = STATES.IDLE
 
 var player = null
+var path = []
 
 export var sight_angle = 45.0
+export var turn_speed = 360.0
 
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
@@ -19,6 +23,7 @@ func _ready():
 				child.connect("hurt", self, "hurt")
 				
 	health_manager.connect("dead", self, "set_state_dead")
+	character_mover.init(self)
 	set_state_idle()
 
 
@@ -39,6 +44,7 @@ func set_state_idle():
 	
 func set_state_chase():
 	cur_state = STATES.CHASE
+	anim_player.play("walk_loop", 0.2)
 	
 func set_state_attack():
 	cur_state = STATES.ATTACK
@@ -46,13 +52,25 @@ func set_state_attack():
 func set_state_dead():
 	cur_state = STATES.DEAD
 	anim_player.play("die")
+	character_mover.freeze()
+	$CollisionShape.disabled = true
 	
 func process_state_idle(delta):
 	if can_see_player():
 		set_state_chase()
 	
 func process_state_chase(delta):
-	pass
+	var player_pos = player.global_transform.origin
+	var our_pos = global_transform.origin
+	path = nav.get_simple_path(our_pos, player_pos)
+	var goal_pos = player_pos
+	if path.size() > 1 :
+		goal_pos = path[1]
+	var dir = goal_pos - our_pos
+	dir.y = 0
+	character_mover.set_move_vec(dir)
+	
+	face_dir(dir,delta)
 	
 func process_state_attack(delta):
 	pass
@@ -81,6 +99,15 @@ func has_los_player():
 		return false
 	return true
 	
+func face_dir(dir: Vector3, delta):
+	var angle_diff = global_transform.basis.z.angle_to(dir)
+	var turn_right = sign(global_transform.basis.x.dot(dir))
+	print(abs(angle_diff) < deg2rad(turn_speed) * delta)
+	if abs(angle_diff) < deg2rad(turn_speed) * delta:
+		rotation.y = atan2(dir.x, dir.z)
+	else:
+		rotation.y += deg2rad(turn_speed) * delta * turn_right
+
 func alert(check_los = true):
 	if cur_state != STATES.IDLE:
 		return
