25c826c7f536278995e68f97e210e69825bcf514
ikilinc1 <ikilinc1@binghamton.edu>
2022-04-28 14:06:34 -0400

    added blood particles

--- Retro-3D-FPS/characters/HealthManager.gd
+++ Retro-3D-FPS/characters/HealthManager.gd
@@ -1,5 +1,7 @@
 extends Spatial
 
+var blood_spray = preload("res://effects/BloodSpray.tscn")
+
 signal dead
 signal hurt
 signal healed
@@ -19,6 +21,8 @@ func init():
 	emit_signal("health_changed", cur_health)
 
 func hurt(damage: int, dir: Vector3):
+	print("blod?")
+	spawn_blood(dir)
 	if cur_health <= 0:
 		return
 	cur_health -= damage
@@ -39,3 +43,25 @@ func heal(amount: int):
 		cur_health = max_health
 	emit_signal("healed")
 	emit_signal("health_changed", cur_health)
+
+func spawn_blood(dir):
+	var blood_spray_inst = blood_spray.instance()
+	get_tree().get_root().add_child(blood_spray_inst)
+	blood_spray_inst.global_transform.origin = global_transform.origin
+		
+	var children : Array = blood_spray_inst.get_children()
+	for child in children:
+		if child is Particles:
+			child.emitting = true
+		
+	if dir.angle_to(Vector3.UP) < 0.00005:
+		return
+	if dir.angle_to(Vector3.DOWN) < 0.00005:
+		blood_spray_inst.rotate(Vector3.RIGHT, PI)
+		return
+			
+	var y = dir
+	var x = y.cross(Vector3.UP)
+	var z = x.cross(y)
+		
+	blood_spray_inst.global_transform.basis = Basis(x, y, z)
